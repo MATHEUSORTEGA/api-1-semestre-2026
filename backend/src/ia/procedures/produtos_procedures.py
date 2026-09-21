f6c834a7f25c3023de7f602a5543c59817406f5e
Felipe Campos <felipemoraisoc@gmail.com>
2026-09-21 12:58:47 -0300

    feat (MT-99): procedure produtos de buscar produtos por setor

--- backend/src/ia/procedures/produtos_procedures.py
+++ backend/src/ia/procedures/produtos_procedures.py
@@ -1,11 +1,20 @@
-# procedures.vendas.buscar_por_nome_produto()
+from pathlib import Path
 import pandas as pd
 
-def buscar_por_nome_produto(nome_produto: str):
-    """Busca um produto pelo nome."""
-    df = pd.read_csv('../dados/produtos.csv')
+path = Path(__file__).resolve().parents[3] / "dados" / "produtos_mercado.csv"
 
-    resultado = df[df['nome'].str.contains(nome_produto, case=False, na=False)]
+def buscar_por_nome_produto(nome_produto: str):
+    df = pd.read_csv(path)
+
+    resultado = df[df['Nome'].str.contains(nome_produto, case=False, na=False)]
 
     return resultado.to_dict(orient='records')
-    
+
+def produto_por_setor(setor: str):
+    df = pd.read_csv(path, sep=";", encoding="utf-8")
+
+    resultado = df[df['Setor'].str.contains(setor, case=False, na=False)]
+
+    return resultado.to_dict(orient='records')
+
+print(produto_por_setor("Padaria"))
