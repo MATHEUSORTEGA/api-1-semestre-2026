2103d3e8ef4f9358388db6089cfb9e3b83510eca
Felipe Campos <felipemoraisoc@gmail.com>
2026-09-21 13:40:34 -0300

    fix (MT-109): apagado funções duplicadas

--- backend/src/ia/procedures/produtos_procedures.py
+++ backend/src/ia/procedures/produtos_procedures.py
@@ -4,22 +4,10 @@ import pandas as pd
 path = Path(__file__).resolve().parents[3] / "dados" / "produtos_mercado.csv"
 prod_tb = pd.read_csv(path, sep=';')
 
-def listar():
+def listar():   
     produtos = prod_tb['Produto'].unique().tolist()
     print(f'[DEBUG] Produtos disponíveis: \n{produtos}')  
     return produtos
-
-def buscar_por_nome_produto(nome_produto: str):
-    df = pd.read_csv(path)
-
-    resultado = df[df['Nome'].str.contains(nome_produto, case=False, na=False)]
-
-    return resultado.to_dict(orient='records')
-
-def produto_por_setor(setor: str):
-    df = pd.read_csv(path, sep=";", encoding="utf-8")
-
-    resultado = df[df['Setor'].str.contains(setor, case=False, na=False)]
 
 def listar_detalhado() -> str:
     return prod_tb.to_string(index=False)
